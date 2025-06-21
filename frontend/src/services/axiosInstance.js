a1136b897aef63bdb5ce4d8331620c41367fcdd0
techdev19 <adilhk1911@gmail.com>
2025-06-21 21:37:38 +0400

    REACT_APP_API_URL

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000', // Adjust base URL as needed
+  // baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000', // Adjust base URL as needed
+  baseURL: process.env.REACT_APP_API_URL, // Adjust base URL as needed
   headers: {
     'Content-Type': 'application/json',
   },
