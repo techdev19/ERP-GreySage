dc5aaabefcd5513f8ed4b2bdd65cf7f340a75134
techdev19 <adilhk1911@gmail.com>
2025-07-03 19:24:37 +0400

    design fix

--- frontend/src/components/Theme/customizations/inputs.js
+++ frontend/src/components/Theme/customizations/inputs.js
@@ -385,7 +385,7 @@ export const inputsCustomizations = {
         color: (theme.vars || theme).palette.text.primary,
         borderRadius: (theme.vars || theme).shape.borderRadius,
         border: `1px solid ${(theme.vars || theme).palette.divider}`,
-        backgroundColor: (theme.vars || theme).palette.background.default,
+        // backgroundColor: (theme.vars || theme).palette.background.default,
         transition: 'border 120ms ease-in',
         '&:hover': {
           borderColor: gray[400],
